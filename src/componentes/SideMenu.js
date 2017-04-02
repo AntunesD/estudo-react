3fd1498c9f6f6a0f12a355ee3be39776ae1fcb71
Diney Luiz Antunes <Diney Luiz Antunes>
2017-04-02 20:46:26 -0300

    deleta componente

--- src/componentes/SideMenu.js
+++ src/componentes/SideMenu.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import Li from "./Commons/Li";
 
 export default class SideMenu extends Component {
-
     render() {
         return (
             <div id="menu">
                 <div className="pure-menu">
                     <a className="pure-menu-heading" href="#">Company</a>
                     <ul className="pure-menu-list">
-                        <Li href="#" label="Home" />
-                        <Li href="#" label="Autor" />
-                        <Li href="#" label="Livro" />
+                        <li className="pure-menu-item">
+                            <a href="#" className="pure-menu-link">Home</a>
+                        </li>
+                        <li className="pure-menu-item">
+                            <a href="#" className="pure-menu-link">Autor</a>
+                        </li>
+                        <li className="pure-menu-item">
+                            <a href="#" className="pure-menu-link">Livro</a>
+                        </li>
                     </ul>
                 </div>
             </div>
